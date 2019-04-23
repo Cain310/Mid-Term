4ee846d40eac82280d5f39f797ddd59d31f6f91f
Michael Cain <michaelcaincb@gmail.com>
2019-04-23 22:40:06 +0000

    initialize github

---
+++ test_script.ts
@@ -0,0 +1,18 @@
+GOOGLE MAPS PROJECT
+https://blog.rapidapi.com/most-popular-apis/
+
+Available PROJECT API'S
+  - Webcams
+  - Nutrition
+  - Space
+  - Airline x2, pricing
+    - Restraunts
+    - Vehicle Tracking
+      - Geo - Location lookup could be an add on
+        - Currency Exchange for rates around the world
+          - UNOGS netflix data api Country / Search / Title info, IMDB
+            - Phone Number area Code Locations
+              - Video and Audio links to top sites.Could be an add on...
+- WebSearch engine api, mimics the way a human brain indexes memories.
+- random famous quotes.
+- Ip GeoLocator based on person location

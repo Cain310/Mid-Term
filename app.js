b4d95bd547273e114137252205f8561ba7bc716e
Your Name <your@email.com>
2019-04-24 23:08:05 +0000

    testing the app.js routs for login and register

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
-var urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.post("/", function(req, res) {
+  res.render("index");
 });
 
 // $(() => {
